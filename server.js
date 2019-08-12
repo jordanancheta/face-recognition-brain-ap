28b58ebcf4d12908c36bda7db46d3e6dd46c3008
Jordan Ancheta <jordanancheta@gmail.com>
2019-08-12 09:59:49 -0700

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image')
 const db = knex({
    client: 'pg',
    connection: {
-     host : 'postgresql-crystalline-26183',
-     user : 'jordanancheta',
-     password : '',
-     database : 'smart-brain'
+     host : process.env.DATABASE_URL,
+     ssl:true,
    }
  });
  
